797e17475b494445c16a2dca86f3a1c21ede7974
Brayden Halliday <brayden@clickspace.com>
2022-03-15 20:09:07 -0600

    Now receiving task list on start and displaying it crudely

--- lib/main.dart
+++ lib/main.dart
@@ -6,44 +6,49 @@ import 'package:http/http.dart' as http;
 
 
 
-Future<Task> fetchTasks() async {
+Future<Tasks> fetchTasks() async {
   final response = await http
       .get(Uri.parse('https://flutter-tasker-server.herokuapp.com/tasks'));
 
   if (response.statusCode == 200) {
-
-    return Task.fromJson(jsonDecode(response.body));
+    return Tasks.fromJson(jsonDecode(response.body));
   } else {
 
     throw Exception('Failed to load tasks');
   }
 }
 
-
 class Task {
+
   final String name;
-/*  final Object createdDate;
+  final Object createdDate;
   final Object startDate;
   final Object completeDate;
-  final Object deleteDate;*/
+  final Object deleteDate;
 
   const Task({
     required this.name,
-    /*required this.createdDate,
+    required this.createdDate,
     required this.startDate,
     required this.completeDate,
-    required this.deleteDate*/
+    required this.deleteDate
+
+  });
+}
+
+
+class Tasks {
+  final List tasks;
+
+  const Tasks({
+    required this.tasks
 
   });
 
-  factory Task.fromJson(List<dynamic> json) {
-    return Task(
+  factory Tasks.fromJson(List <dynamic> json) {
+    return Tasks(
       //todo: populate 2 tasks with postMaster, access them properly,display list
-      name: json[0],
-    /*  createdDate: json['createdDate'],
-      startDate: json['startDate'],
-      completeDate: json['completeDate'],
-      deleteDate: json['deleteDate'],*/
+      tasks: json,
     );
   }
 }
@@ -60,12 +65,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<Task> futureTask;
+  late Future<Tasks> futureTasks;
 
   @override
   void initState() {
     super.initState();
-    futureTask = fetchTasks();
+    futureTasks = fetchTasks();
   }
 
   @override
@@ -80,11 +85,11 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Tasker'),
         ),
         body: Center(
-          child: FutureBuilder<Task>(
-            future: futureTask,
+          child: FutureBuilder<Tasks>(
+            future: futureTasks,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Text(snapshot.data!.name);
+                return Text(snapshot.data!.tasks.toString());
               } else if (snapshot.hasError) {
                 return Text('${snapshot.error}');
               }
