068568157fa7f661350dd0d5c3e954a58b0bb214
Brayden Halliday <brayden@clickspace.com>
2022-03-29 19:12:26 -0600

    cleaned up comments, reset popup approuch

--- lib/main.dart
+++ lib/main.dart
@@ -125,98 +125,10 @@ class _MyAppState extends State<MyApp> {
           height: 60,
           color: Colors.black12,
           child: InkWell(
-            onTap: () {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      content: Stack(
-                        overflow: Overflow.visible,
-                        children: <Widget>[
-                          Positioned(
-                            right: -40.0,
-                            top: -40.0,
-                            child: InkResponse(
-                              onTap: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: CircleAvatar(
-                                child: Icon(Icons.close),
-                                backgroundColor: Colors.red,
-                              ),
-                            ),
-                          ),
-                          Form(
-                            key: _formKey,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: TextFormField(),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: TextFormField(),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: RaisedButton(
-                                    child: Text("Submitß"),
-                                    onPressed: () {
-                                      if (_formKey.currentState!.validate()) {
-                                        _formKey.currentState!.save();
-                                      }
-                                    },
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
-
-
-              child:
-              Padding(
-                padding: EdgeInsets.only(top: 8.0),
-                child: Column(
-                  children: <Widget>[
-                    Icon(
-                      Icons.add_box_outlined,
-                      color: Theme
-                          .of(context)
-                          .accentColor,
-                    ),
-                    Text('New Task'),
-
-                  ],
-                ),
-              );
-            }),
+            onTap: () => print('temporary - will be a function'),
         ),
 
-
-   /*     bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add_box_outlined),
-              label: 'New',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.camera),
-              label: 'Delete',
-            ),
-
-          ],
-        ),*/
-
-
-
-
       ),
-    );
+    ));
   }
 }
